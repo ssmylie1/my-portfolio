af0fbcc044c56f1e85e2f093ec5354ee48439857
Samantha Smylie <smyliesamantha@gmail.com>
2018-10-10 00:05:05 -0400

    changing profile again.

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+<script type="text/javascript">
+  $(document).ready(function() {
+
+  $('.header a').click(function(e) {
+    e.preventDefault();
+    var curInx = this.hash;
+    var target = $(curInx);
+    var targetOffset = target.offset().top;
+
+    $("body").animate({scrollTop:targetOffset}, 400, function() {
+      location.hash = curInx;
+    });
+
+
+  });
+
+
+  $(window).scroll(function() {
+      var sticky = function(index) {
+        $('li').removeClass('active'), $('.' + index + '-nav').addClass('active');
+      };
+    $('#container > div').each(function(){
+      var cur = $(this);
+      var curTop = cur.offset().top;
+      var curInx = cur.attr("id");
+      var sidebarTop = $('.'+curInx+'-nav').offset().top +60;
+        if (sidebarTop > curTop) {
+          sticky(curInx);
+        }
+    });
+  });
+
+  });
+
+
